46e44be18963c59384ea53dd3b1d290d12100de6
elbrus998 <elbrus98@wp.pl>
2023-12-15 00:47:39 +0100

    .

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -9,26 +9,25 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private CharacterSheet characterSheet;
     [SerializeField] private GameManager gameManager;
     private bool isDead = false;
-    [SerializeField] private int MaxSpeed = 10;
-    private int CurrentSpeed;
+    [SerializeField] private int MaxMoveDistance = 10;
+    private int MoveDistance;
     [SerializeField] private int MaxAttackPoints = 1;
     private int AttackPoints;
     [SerializeField] private int Damage = 3;
     [SerializeField] private int AttackRange = 25;
 
     private NavMeshAgent agent;
-    private bool hasAttacked = false;
 
     void Start()
     {
         currentHealth = characterSheet.MaxHealth;
-        CurrentSpeed = MaxSpeed;
         AttackPoints = MaxAttackPoints;
+        MoveDistance = MaxMoveDistance;
 
         agent = GetComponent<NavMeshAgent>();
         if (agent != null)
         {
-            agent.speed = MaxSpeed;
+            agent.speed = 5;
         }
     }
 
@@ -52,72 +51,24 @@ public class EnemyController : MonoBehaviour
     {
         if (gameManager.isFight == false || characterSheet.PlayerTurn != gameManager.Turn)
         {
-            CurrentSpeed = MaxSpeed;
+            MoveDistance = MaxMoveDistance;
             AttackPoints = MaxAttackPoints;
         }
 
-            if (gameManager.isFight && characterSheet.PlayerTurn == gameManager.Turn)
+        if (gameManager.isFight && characterSheet.PlayerTurn == gameManager.Turn)
         {
-            GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-            Transform nearestPlayer = null;
-            float nearestDistance = Mathf.Infinity;
-
-            foreach (GameObject player in players)
-            {
-                float distance = Vector3.Distance(transform.position, player.transform.position);
-                if (distance < nearestDistance)
-                {
-                    nearestDistance = distance;
-                    nearestPlayer = player.transform;
-                }
-            }
-
-            if (nearestPlayer != null && nearestDistance <= AttackRange)
-            {
-                if (agent != null && agent.isOnNavMesh && agent.isStopped && AttackPoints > 0)
-                {
-                    float distanceToPlayer = Mathf.Min(nearestDistance, CurrentSpeed);
-                    Vector3 destination = transform.position + (nearestPlayer.position - transform.position).normalized * distanceToPlayer;
-                    agent.SetDestination(destination);
-
-                    // Sprawdzanie zasiêgu ataku po osi¹gniêciu docelowego punktu
-                    if (distanceToPlayer <= AttackRange)
-                    {
-                        AttackPlayer(nearestPlayer);
-                        AttackPoints--;
-                    }
-                }
-            }
+           
         }
     }
 
-    void AttackPlayer(Transform player)
+    void Moving()
     {
-        for (int i = 0; i < AttackPoints; i++)
-        {
-            Vector3 direction = (player.position - transform.position).normalized;
 
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, direction, out hit, AttackRange))
-            {
-                if (hit.collider.CompareTag("Player"))
-                {
-                    Movement playerMovement = hit.collider.GetComponent<Movement>();
-                    if (playerMovement != null)
-                    {
-                        playerMovement.TakeDamage(Damage);
-                        Debug.Log("Enemy dealt " + Damage + " damage to the player.");
-                    }
-                }
-            }
-            else
-            {
-                Debug.Log("Enemy raycast didn't hit the player.");
-            }
-
-            Debug.DrawRay(transform.position, direction * AttackRange, Color.green, 1.0f);
-        }
     }
 
+    void Attack()
+    {
+
+    }
 }
 
